ce984a4bd7e8bc8330e7539aaceffd483c0f7d6a
Willu12 <boryskurdek@gmail.com>
2023-12-06 03:16:04 +0100

    changed file structure

---
+++ src/scan.cu
@@ -0,0 +1,134 @@
+#include <stdlib.h>
+//#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "kernels.cuh"
+#include "scan.cuh"
+#include "stdio.h"
+
+#define checkCudaError(o, l) _checkCudaError(o, l, __func__)\
+
+int THREADS_PER_BLOCK = 512;
+int ELEMENTS_PER_BLOCK = THREADS_PER_BLOCK * 2;
+
+
+int nextPowerOfTwo(int x) {
+	int power = 1;
+	while (power < x) {
+		power *= 2;
+	}
+	return power;
+}
+
+float blockscan(int *output, int *input, int length){
+	int *d_out, *d_in;
+	const int arraySize = length * sizeof(int);
+
+	cudaMalloc((void **)&d_out, arraySize);
+	cudaMalloc((void **)&d_in, arraySize);
+	cudaMemcpy(d_out, output, arraySize, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_in, input, arraySize, cudaMemcpyHostToDevice);
+
+	// start timer
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start);
+
+	int powerOfTwo = nextPowerOfTwo(length);
+	prescan_arbitrary<<<1, (length + 1) / 2, 2 * powerOfTwo * sizeof(int)>>>(d_out, d_in, length, powerOfTwo);
+
+	// end timer
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	float elapsedTime = 0;
+	cudaEventElapsedTime(&elapsedTime, start, stop);
+
+	cudaMemcpy(output, d_out, arraySize, cudaMemcpyDeviceToHost);
+
+	cudaFree(d_out);
+	cudaFree(d_in);
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	return elapsedTime;
+}
+
+float scan(int *output, int *input, int length) {
+
+	// start timer
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start);
+
+    if (length > ELEMENTS_PER_BLOCK) {
+		scanLargeDeviceArray(output, input, length);
+	}
+	else {
+		scanSmallDeviceArray(output, input, length);
+	}
+
+
+    // end timer
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	float elapsedTime = 0;
+	cudaEventElapsedTime(&elapsedTime, start, stop);
+
+    
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	return elapsedTime;
+}
+
+
+void scanLargeDeviceArray(int *d_out, int *d_in, int length) {
+	int remainder = length % (ELEMENTS_PER_BLOCK);
+	if (remainder == 0) {
+		scanLargeEvenDeviceArray(d_out, d_in, length);
+	}
+	else {
+		// perform a large scan on a compatible multiple of elements
+		int lengthMultiple = length - remainder;
+        scanLargeEvenDeviceArray(d_out, d_in, lengthMultiple);
+		// scan the remaining elements and add the (inclusive) last element of the large scan to this
+		int *startOfOutputArray = &(d_out[lengthMultiple]);
+		scanSmallDeviceArray(startOfOutputArray, &(d_in[lengthMultiple]), remainder);
+
+        add<<<1, remainder>>>(startOfOutputArray, remainder, &(d_in[lengthMultiple - 1]), &(d_out[lengthMultiple - 1]));
+	}
+}
+
+void scanSmallDeviceArray(int *d_out, int *d_in, int length) {
+	int powerOfTwo = nextPowerOfTwo(length);
+	prescan_arbitrary <<<1, (length + 1) / 2, 2 * powerOfTwo * sizeof(int) >>>(d_out, d_in, length, powerOfTwo);
+}
+
+void scanLargeEvenDeviceArray(int *d_out, int *d_in, int length) {
+	const int blocks = length / ELEMENTS_PER_BLOCK;
+	const int sharedMemArraySize = ELEMENTS_PER_BLOCK * sizeof(int);
+
+	int *d_sums, *d_incr;
+	cudaMalloc((void **)&d_sums, blocks * sizeof(int));
+	cudaMalloc((void **)&d_incr, blocks * sizeof(int));
+
+
+    prescan_large<<<blocks, THREADS_PER_BLOCK, 2 * sharedMemArraySize>>>(d_out, d_in, ELEMENTS_PER_BLOCK, d_sums);
+
+
+    const int sumsArrThreadsNeeded = (blocks + 1) / 2;
+
+    if (sumsArrThreadsNeeded > THREADS_PER_BLOCK) {
+        scanLargeDeviceArray(d_incr, d_sums, blocks);
+    }
+    else {
+        scanSmallDeviceArray(d_incr, d_sums, blocks);
+    }
+
+    add<<<blocks, ELEMENTS_PER_BLOCK>>>(d_out, ELEMENTS_PER_BLOCK, d_incr);
+
+	cudaFree(d_sums);
+	cudaFree(d_incr);
+}
